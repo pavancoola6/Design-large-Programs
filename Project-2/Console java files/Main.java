a27f751f2a375adbf0c357898105324f70e1f4df
Pavan Kumar Singara <punmcse@unm.edu>
2021-03-01 06:50:45 +0000

    Upload New File

---
+++ Project-2/Console java files/Main.java
@@ -0,0 +1,13 @@
+package domino;
+
+public class Main {
+
+
+    //main method
+    public static void main(String[] args) {
+
+        ConsoleDominoGame game = new ConsoleDominoGame();
+        game.run();
+
+    }
+}
