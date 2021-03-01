e40bf696401a0c9df2bc8875a983b15987897b79
Pavan Kumar Singara <punmcse@unm.edu>
2021-03-01 06:58:07 +0000

    Upload New File

---
+++ Project-2/GUI java file/Domino.java
@@ -0,0 +1,34 @@
+package domino;
+
+
+public class Domino {
+
+
+    private int[] values;
+
+
+    public Domino(int first, int second){
+        values = new int[]{first, second};
+    }
+
+    public int getFirst(){
+        return values[0];
+    }
+    public int getSecond(){
+        return values[1];
+    }
+    public int getTotal(){
+
+        //special case: 0 - 0
+        if (values[0] == values[1] && values[0] == 0){
+            return 12;
+        }
+
+        return values[0] + values[1];
+    }
+
+    @Override
+    public String toString() {
+        return values[0] + " - " + values[1];
+    }
+}
