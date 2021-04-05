a94316dcb0a74feb25542bfcd4f16efa3feb4f88
Pavan Kumar Singara <punmcse@unm.edu>
2021-04-05 09:17:30 +0000

    Upload New File

---
+++ Project3/src/Location.java
@@ -0,0 +1,18 @@
+package ScrabbleGame.Project3.model;
+
+
+public class Location {
+
+    //row
+    public int x;
+
+    //column
+    public int y;
+
+
+    public Location(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
