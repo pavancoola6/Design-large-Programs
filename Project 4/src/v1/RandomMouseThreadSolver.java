2c42ec6b57bf5d54a8bdc23530db49cce372c367
Pavan Kumar Singara <punmcse@unm.edu>
2021-04-22 22:35:22 +0000

    Upload New File

---
+++ Project 4/src/v1/RandomMouseThreadSolver.java
@@ -0,0 +1,52 @@
+/* Authors - Pavan Kumar Singara and Anmol Singh Gill
+ * CS351L - Project 4
+ *
+ */
+
+package v1;
+
+import java.util.List;
+import java.util.Set;
+
+import javafx.application.Platform;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class RandomMouseThreadSolver implements MazeSolver{
+	private Cell end;
+	private Cell current;
+	private String direction;
+	
+	public void solveMaze(Board board, Cell start, Cell end, int delay, GraphicsContext gc, Stage stage, int cellSize, int winSize, int rendSize, boolean rendSelf) {
+		this.end = end;
+		current = start;
+		direction = board.getRandPossDirection(current);
+		
+		solveLoop(board, delay, gc, stage, cellSize, winSize, rendSize, rendSelf);
+	}
+
+	@Override
+	public boolean generateStep(Board board) {
+		if (current == end)
+			return false;
+		if (board.pathOpen(current, board.getTurnedDirection(direction, 2)) && board.getPossDirections(current).size() > 1) {
+			String newDir = board.getRandPossDirection(current);
+			while(newDir.equals(board.getTurnedDirection(direction, 2))) {
+				newDir = board.getRandPossDirection(current);
+			}
+			direction = newDir;
+			current = board.getRelative(current, direction);
+		}
+		else {
+			direction = board.getRandPossDirection(current);
+			current = board.getRelative(current, direction);
+		}
+		//Still solving? Yes
+		return true;
+	}
+
+	public void render(GraphicsContext gc, int cellSize, int winSize, int rendSize, Color cellCol, Color wallCol) {
+		current.render(gc, cellSize, winSize, rendSize, cellCol, wallCol);	
+	}
+}
